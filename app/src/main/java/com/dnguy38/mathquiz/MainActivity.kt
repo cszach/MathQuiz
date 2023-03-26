18d6840ba6ea027154fd804f850d04973ce93900
Duong (Zach) Nguyen <novak@novakcgx.me>
2023-03-25 20:47:17 -0400

    Initial commit

---
+++ app/src/main/java/com/dnguy38/mathquiz/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.dnguy38.mathquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dnguy38.mathquiz.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
