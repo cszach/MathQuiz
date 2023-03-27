c3121785f4141f7b95aa25b43bcbd727d43fa9c4
Duong (Zach) Nguyen <novak@novakcgx.me>
2023-03-26 22:13:49 -0400

    [SettingsFragment] View

--- app/src/main/java/com/dnguy38/mathquiz/SettingsFragment.kt
+++ app/src/main/java/com/dnguy38/mathquiz/SettingsFragment.kt
@@ -1,11 +1,25 @@
 package com.dnguy38.mathquiz
 
 import android.os.Bundle
-import androidx.preference.PreferenceFragmentCompat
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 
-class SettingsFragment : PreferenceFragmentCompat() {
+class SettingsFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
-    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = SettingsFragment()
     }
 }
