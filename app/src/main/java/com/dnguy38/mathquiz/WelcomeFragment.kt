583247fd5ed25a2e30f5fa8cae13e02c1899c86a
Duong (Zach) Nguyen <novak@novakcgx.me>
2023-03-27 00:33:58 -0400

    [WelcomeFragment] Navigation to other fragments

--- app/src/main/java/com/dnguy38/mathquiz/WelcomeFragment.kt
+++ app/src/main/java/com/dnguy38/mathquiz/WelcomeFragment.kt
@@ -5,21 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
 
 class WelcomeFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var newGameButton: Button
+    private lateinit var settingsButton: Button
+    private lateinit var statsButton: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_welcome, container, false)
 
-    companion object {
-        @JvmStatic
-        fun newInstance() = WelcomeFragment()
+        newGameButton = view.findViewById(R.id.new_game_button)
+        newGameButton.setOnClickListener {
+            view.findNavController().navigate(R.id.action_welcomeFragment_to_configurationFragment)
+        }
+
+        settingsButton = view.findViewById(R.id.settings_button)
+        settingsButton.setOnClickListener {
+            view.findNavController().navigate(R.id.action_welcomeFragment_to_settingsFragment)
+        }
+
+        statsButton = view.findViewById(R.id.stats_button)
+        statsButton.setOnClickListener {
+            view.findNavController().navigate(R.id.action_welcomeFragment_to_statisticsFragment)
+        }
+
+        return view
     }
 }
